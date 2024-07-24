f35709e1ee202a6b4d61c1f8346bd7566e82a69b
Craig Donnelly <crag@redhat.com>
2024-07-24 09:42:52 -0400

    Update dep - reporter to 0.4.0

--- project.clj
+++ project.clj
@@ -21,7 +21,7 @@
     [io.vertx/vertx-auth-oauth2 "3.5.0"]
     [io.reactivex.rxjava2/rxjava "2.1.6"]
     [org.slf4j/slf4j-simple "1.7.36"]	
-    [com.github.redhatqe/polarizer-reporter "0.3.0-SNAPSHOT"]
+    [com.github.redhatqe/polarizer-reporter "0.4.0-SNAPSHOT"]
     [com.github.redhatqe/polarizer-polarizer "0.3.1-SNAPSHOT"]]
   :javac-options {:debug "on"}
   :plugins [[lein2-eclipse "2.0.0"]]
