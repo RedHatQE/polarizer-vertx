ec19218c833e84f8abe698c3952d57a7bdea5b99
Craig Donnelly <crag@redhat.com>
2024-07-24 09:20:45 -0400

    0.3.1 - Update LICENSE, bump version for stack rebuild

--- project.clj
+++ project.clj
@@ -1,6 +1,11 @@
-(defproject com.github.redhatqe/polarizer-vertx "0.3.0-SNAPSHOT"
+(defproject com.github.redhatqe/polarizer-vertx "0.3.1-SNAPSHOT"
   :description "A service to upload data to Polarion"
   :url "https://github.com/RedHatQE/polarizer-vert"
+  :license {:name "GPL-3.0"
+            :comment "GNU General Public License v3.0"
+            :url "https://choosealicense.com/licenses/gpl-3.0"
+            :year 2024
+            :key "gpl-3.0"}
   :java-source-path "src"
   :java-source-paths ["src"]
   :main io.vertx.core.Launcher
@@ -17,7 +22,7 @@
     [io.reactivex.rxjava2/rxjava "2.1.6"]
     [org.slf4j/slf4j-simple "1.7.36"]	
     [com.github.redhatqe/polarizer-reporter "0.3.0-SNAPSHOT"]
-    [com.github.redhatqe/polarizer-polarizer "0.3.0-SNAPSHOT"]]
+    [com.github.redhatqe/polarizer-polarizer "0.3.1-SNAPSHOT"]]
   :javac-options {:debug "on"}
   :plugins [[lein2-eclipse "2.0.0"]]
   :profiles {:uberjar {:aot :all}})
